b155229554c4e0cb143b9046122632e676987502
Goran Simic <goransimicdev@gmail.com>
2024-07-18 13:37:04 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 echo "Installing IR Emitter..."
 sudo apt-get update > /dev/null
 sudo apt-get install -y git > /dev/null
-git clone https://github.com/goransimic/ir-emitter.git /tmp/ir-emitter >/dev/null
+git clone https://github.com/goransimic/ir-emitter.git /tmp/ir-emitter > /dev/null
 sudo cp /tmp/ir-emitter/ir-emitter /usr/local/bin/
 sudo cp /tmp/ir-emitter/ir-emitter.service /etc/systemd/system/
-sudo systemctl enable --now ir-emitter >/dev/null
+sudo systemctl enable --now ir-emitter > /dev/null
 rm -rf /tmp/ir-emitter
