a2b0054d6aa38d69cd337440d491f82768653eb3
Goran Simic <goransimicdev@gmail.com>
2024-07-18 12:50:57 +0200

    Update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Uninstalling IR Emitter..."
-sudo systemctl disable --now ir-emitter &> /dev/null
+sudo systemctl disable --now ir-emitter > /dev/null
 sudo rm -rf /etc/systemd/system/ir-emitter.service
 sudo rm -rf /usr/local/bin/ir-emitter
